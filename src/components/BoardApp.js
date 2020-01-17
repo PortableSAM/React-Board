0bafbe4c5105fba6167a84098eff32f3d2c109cf
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2020-01-17 14:26:19 +0900

    DB내용 불러오기

---
+++ src/components/BoardApp.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import fire_Config from "./fireConfig/config";
+
+const firebase = require("firebase/app");
+require("firebase/firestore");
+
+firebase.initializeApp(fire_Config);
+
+function BoardApp() {
+  const [brdItem, setBrdItem] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      const db = firebase.firestore();
+      //fireStore 실시간 리스너
+      db.collection("Post").onSnapshot(snapshot => {
+        const resData = snapshot.docs.map(doc => ({
+          ...doc.data(),
+          id: doc.id
+        }));
+        //snapshot 데이터 넘김 (to setBrdItem)
+        setBrdItem(resData);
+      });
+    };
+    return fetchData();
+  }, []);
+
+  console.log(brdItem);
+  return (
+    <div className="container">
+      <div className="table-title">
+        <h2>Hello React FireStore</h2>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>Desc</th>
+            <th>Author</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {brdItem.map(brd => (
+            <tr key={brd.id}>
+              <td>{brd.Title}</td>
+              <td>{brd.Text}</td>
+              <td>{brd.Author}</td>
+              <td>
+                {new Date(brd.CreateAt.seconds * 1000).toLocaleDateString("ko")}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default BoardApp;
---
+++ src/components/fireConfig/config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const Config = {
+  apiKey: "AIzaSyCRabvFBqz1CqP1vMUpnr2v1rPPDLr15oY",
+  authDomain: "react-board-e91cf.firebaseapp.com",
+  databaseURL: "https://react-board-e91cf.firebaseio.com",
+  projectId: "react-board-e91cf",
+  storageBucket: "react-board-e91cf.appspot.com",
+  messagingSenderId: "926371156874",
+  appId: "1:926371156874:web:221e4c1470f6d5599e6e73"
+};
+// Initialize Firebase
+
+export default Config;
